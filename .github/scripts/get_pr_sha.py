e8cf5caf13da21b3c9c4fcba2d62b49044ba1895
Daniel Suo <danielsuo@gmail.com>
2021-12-27 17:09:02 -0800

    blah

--- .github/scripts/get_pr_sha.py
+++ .github/scripts/get_pr_sha.py
@@ -9,6 +9,7 @@ def main():
     parser = argparse.ArgumentParser(description="Get SHA of PR from GitHub.")
     parser.add_argument("repo", type=str, help="repo name")
     parser.add_argument("num", type=int, help="pr number")
+    parser.add_argument("sha_path", type=str, default="/tmp/sha", help="where to write sha")
 
     args = parser.parse_args()
 
@@ -19,7 +20,7 @@ def main():
     with urllib.request.urlopen(url) as response:
         sha = json.loads(response.read().decode())["head"]["sha"]
 
-    with open("/tmp/sha", "w", encoding="UTF-8") as f:
+    with open(args.sha_path, "w", encoding="UTF-8") as f:
         f.write(sha)
 
 
